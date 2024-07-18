f8e2ab06aae32f89d4faa6a1e1a57b470b201b20
ADE_ABDULMALIK <163132381+KingMaleekLamar@users.noreply.github.com>
2024-07-18 02:01:27 +0100

    Delete Hotel Reservation Analysis_ADEDOKUN.sql

--- Hotel Reservation Analysis_ADEDOKUN.sql
+++
@@ -1,6 +0,0 @@
-SELECT to_char(DATE_TRUNC('month', arrival_date), 'Month') AS Month_Name,
-COUNT (booking_id) AS Total_reservations_2018
-FROM hotel_reservation
-	WHERE arrival_date >'2017-12-31'
-GROUP BY Month_Name
-ORDER BY Total_reservations_2018 DESC 
